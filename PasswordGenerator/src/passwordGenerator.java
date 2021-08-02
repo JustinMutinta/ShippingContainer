2ef562c4200891e2f1c84d5e15db4aa6ece9ad4c
JustinMutinta <justinmutinta@hotmail.com>
2021-08-02 14:39:33 -0500

    1st Pass. Basic set up and additions

--- PasswordGenerator/src/passwordGenerator.java
+++ PasswordGenerator/src/passwordGenerator.java
@@ -1,2 +1,32 @@
-public class passwordGenerator {
+import javax.swing.*;
+
+public class passwordGenerator extends JFrame {
+
+    JPanel passPanel;
+    JButton closeButton;
+    JButton generateButton;
+    JTextField passwordBox;
+
+    public passwordGenerator(){
+        setSize(600, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        passPanel = new JPanel();
+        closeButton = new JButton("Close");
+        generateButton = new JButton("Generate");
+        passwordBox = new JTextField();
+
+        passPanel.add(closeButton);
+        passPanel.add(generateButton);
+        passPanel.add(passwordBox);
+
+        add(passPanel);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new passwordGenerator();
+    }
+
 }
