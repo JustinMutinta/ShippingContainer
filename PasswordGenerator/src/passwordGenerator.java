1f5f9f4877c1fb1d23f985f72ed8caa55d073eb2
JustinMutinta <justinmutinta@hotmail.com>
2021-08-02 15:10:54 -0500

    3rd Pass. First button complete. Added Comments

--- PasswordGenerator/src/passwordGenerator.java
+++ PasswordGenerator/src/passwordGenerator.java
@@ -1,27 +1,29 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class passwordGenerator extends JFrame {
+public class passwordGenerator extends JFrame {  //New class that extends JFrame
 
-    JPanel passPanel;
-    JButton closeButton;
-    JButton generateButton;
-    JTextField passwordBox;
+    JPanel passPanel;       //Create new Panel to hold objects
+    JButton closeButton;    //Create new Button to close window
+    JButton generateButton; //Create new Button to generate password
+    JTextField passwordBox; //TextBox to hold password when Generated.
 
-    public passwordGenerator(){
-        setSize(600, 100);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public passwordGenerator(){                             //GUI set up
+        setSize(600, 100);                      //set up Dimensions of GUI
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     //Close GUI when X is clicked.
 
-        passPanel = new JPanel();
-        passPanel.setLayout(null);
-        passPanel.setSize(600,100);
+        passPanel = new JPanel();                           //initialize Panel
+        passPanel.setLayout(null);                          //set Layout of Panel to null. Allows me to customize the panel
+        passPanel.setSize(600,100);             //Set size of Panel
 
-        closeButton = new JButton("Close");
+        closeButton = new JButton("Close");             //initialize Buttons/TextField and assign labels
         generateButton = new JButton("Generate");
         passwordBox = new JTextField();
 
-        Insets insets = getInsets();
-        Dimension size;
+        Insets insets = getInsets();                        //To customize placement
+        Dimension size;                                     //To customize size
 
         size = closeButton.getPreferredSize();
         closeButton.setBounds(500 + insets.left, 25 + insets.top, size.width + 10, size.height);
@@ -32,13 +34,20 @@ public class passwordGenerator extends JFrame {
         size = passwordBox.getPreferredSize();
         passwordBox.setBounds(10 + insets.left, 25 + insets.top, size.width + 350, size.height + 8);
 
-        passPanel.add(closeButton);
+        passPanel.add(closeButton);                         //Add objects to panel
         passPanel.add(generateButton);
         passPanel.add(passwordBox);
 
-        add(passPanel);
+        add(passPanel);                             //Add Panel to Frame
 
-        setVisible(true);
+        closeButton.addActionListener(new ActionListener() { //Set action to when close button is closed.
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        setVisible(true); //Make the GUI visible
     }
 
     public static void main(String[] args) {
