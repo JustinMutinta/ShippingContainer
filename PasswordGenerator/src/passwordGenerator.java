bfc0c2e6c690610ab0cffd5c1ef84821bc94b6a2
JustinMutinta <justinmutinta@hotmail.com>
2021-08-02 14:53:53 -0500

    2nd Pass. Presentation complete

--- PasswordGenerator/src/passwordGenerator.java
+++ PasswordGenerator/src/passwordGenerator.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class passwordGenerator extends JFrame {
 
@@ -12,9 +13,24 @@ public class passwordGenerator extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         passPanel = new JPanel();
+        passPanel.setLayout(null);
+        passPanel.setSize(600,100);
+
         closeButton = new JButton("Close");
         generateButton = new JButton("Generate");
         passwordBox = new JTextField();
+
+        Insets insets = getInsets();
+        Dimension size;
+
+        size = closeButton.getPreferredSize();
+        closeButton.setBounds(500 + insets.left, 25 + insets.top, size.width + 10, size.height);
+
+        size = generateButton.getPreferredSize();
+        generateButton.setBounds(400 + insets.left, 25 + insets.top, size.width + 10, size.height);
+
+        size = passwordBox.getPreferredSize();
+        passwordBox.setBounds(10 + insets.left, 25 + insets.top, size.width + 350, size.height + 8);
 
         passPanel.add(closeButton);
         passPanel.add(generateButton);
