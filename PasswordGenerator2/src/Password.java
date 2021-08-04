c7aac5b5d43f525cbf374321ffb14dea8013bafe
JustinMutinta <justinmutinta@hotmail.com>
2021-08-04 12:17:32 -0500

    Third Pass. Added function for options of passwords generated

--- PasswordGenerator2/src/Password.java
+++ PasswordGenerator2/src/Password.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class Password extends JFrame {
 
@@ -35,9 +38,54 @@ public class Password extends JFrame {
         passwordComboBox.setBounds(500 + insets.left, 120 + insets.top, size.width + 5, size.height);
         passwordPanel.add(passwordComboBox);
 
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
 
 
         add(passwordPanel);
         setVisible(true);
+    }
+
+
+    public String createPassword(int num){
+        String[] word1 = {"Coffee", "Tea", "Cookie", "Barrista", "Sweetened", "Calories", "Shaken", "Stirred", "Drivethrough"};
+        String[] word2 = {"Sports", "Trophy", "Winner", "Draw", "Loser", "Coach", "Player", "Field", "Arena"};
+        int[] numberValue = {0,1,2,3,4,5,6,7,8,9};
+        char[] symbolValue = {'!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '='};
+
+        Random randomValue = new Random();
+
+        /*
+        level 1 = one word
+        level 2 = one word 2 numbers
+        level 3 = one word 2 numbers one word
+        level 4 = one word 2 numbers one word 2 symbols
+         */
+        switch (num){
+            case 4:
+                return word1[randomValue.nextInt(word1.length)] +              //Assign word1
+                        numberValue[randomValue.nextInt(numberValue.length)] +      //Assign numberValue
+                        numberValue[randomValue.nextInt(numberValue.length)] +      //Assign numberValue
+                        word2[randomValue.nextInt(word2.length)] +              //Assign word2
+                        symbolValue[randomValue.nextInt(symbolValue.length)] +      //Assign symbol
+                        symbolValue[randomValue.nextInt(symbolValue.length)];       //Assign symbol
+            case 3:
+                return word1[randomValue.nextInt(word1.length)] +
+                        numberValue[randomValue.nextInt(numberValue.length)] +
+                        numberValue[randomValue.nextInt(numberValue.length)] +
+                        word2[randomValue.nextInt(word2.length)] +
+                        symbolValue[randomValue.nextInt(symbolValue.length)];
+            case 2:
+                return word1[randomValue.nextInt(word1.length)] +
+                        numberValue[randomValue.nextInt(numberValue.length)] +
+                        numberValue[randomValue.nextInt(numberValue.length)] +
+                        word2[randomValue.nextInt(word2.length)];
+            default:
+                return word1[randomValue.nextInt(word1.length)];
+        }
     }
 }
