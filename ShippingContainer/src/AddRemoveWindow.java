8d12b3ae68091213589894fd1d812bbe97f7d97a
JustinMutinta <justinmutinta@hotmail.com>
2021-08-06 11:47:24 -0500

    3rd pass. Added buttons to windows

--- ShippingContainer/src/AddRemoveWindow.java
+++ ShippingContainer/src/AddRemoveWindow.java
@@ -1,9 +1,37 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AddRemoveWindow extends JFrame {
     public AddRemoveWindow(){
         setSize(300,300);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);  //closes this window without closing the others
+
+        JPanel addRemovePanel = new JPanel();
+        
+
+        JTextArea textAreaLeft = new JTextArea("Left");
+        JTextArea textAreaRight = new JTextArea("Right");
+
+        JButton addButton = new JButton("Add >>");
+        JButton removeButton = new JButton("Remove <<");
+        JButton closeButton = new JButton("Close");
+
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Want it to close only this window but not the main one
+            }
+        });
+
+        addRemovePanel.add(closeButton);
+        addRemovePanel.add(textAreaLeft);
+        addRemovePanel.add(textAreaRight);
+
+
+        add(addRemovePanel);
+
+
 
         setVisible(true);
     }
--- ShippingContainer/src/Container.java
+++ ShippingContainer/src/Container.java
@@ -1,4 +1,36 @@
 public class Container{
-    
+    int height;
+    int width;
+    int length;
+    int weight;
+    String description;
+    String serialNumber;
+
+    public Container(String description, String serialNumber){  //20 foot container
+        this.height = 10;
+        this.width = 10;
+        this.length = 20;
+        this.weight = 4800;
+        this.description = description;
+        this.serialNumber = serialNumber;
+    }
+
+    public Container(String description){
+        this.height = 10;
+        this.width = 10;
+        this.length = 10;
+        this.weight = 5;
+        this.description = description;
+        serialNumber = "No Serial number";
+    }
+
+    public Container(int height, int width, int length, int weight,String description, String serialNumber){
+        this.height = height;
+        this.width = width;
+        this.length = length;
+        this.weight = weight;
+        this.description = description;
+        this.serialNumber = serialNumber;
+    }
 
 }
