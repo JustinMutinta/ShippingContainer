ea20b00c62af93f6dc1f390aaf708758141f18f4
JustinMutinta <justinmutinta@hotmail.com>
2021-08-07 17:51:43 -0500

    5th Pass. Worked more on Add/Remove window

--- ShippingContainer/src/AddRemoveWindow.java
+++ ShippingContainer/src/AddRemoveWindow.java
@@ -1,13 +1,15 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddRemoveWindow extends JFrame {
     public AddRemoveWindow(){
-        setSize(300,300);
+        setSize(500,300);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);  //closes this window without closing the others
 
         JPanel addRemovePanel = new JPanel();
+        addRemovePanel.setLayout(null);
 
 
         JTextArea textAreaLeft = new JTextArea("Left");
@@ -17,6 +19,23 @@ public class AddRemoveWindow extends JFrame {
         JButton removeButton = new JButton("Remove <<");
         JButton closeButton = new JButton("Close");
 
+        Insets insets = getInsets();
+        Dimension size;
+
+        size = textAreaLeft.getPreferredSize();
+        textAreaLeft.setBounds(5 + insets.left, 25 + insets.top, size.width + 100, size.height + 150);
+
+        size = textAreaRight.getPreferredSize();
+        textAreaRight.setBounds(310 + insets.left, 25 + insets.top, size.width + 100, size.height + 150);
+
+        size = addButton.getPreferredSize();
+        addButton.setBounds(130 + insets.left, 25 + insets.top, size.width + 100, size.height);
+
+        size = removeButton.getPreferredSize();
+        removeButton.setBounds(130 + insets.left, 75 + insets.top, size.width + 100, size.height);
+
+
+
         closeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -24,9 +43,11 @@ public class AddRemoveWindow extends JFrame {
             }
         });
 
-        addRemovePanel.add(closeButton);
+        //addRemovePanel.add(closeButton);
         addRemovePanel.add(textAreaLeft);
         addRemovePanel.add(textAreaRight);
+        addRemovePanel.add(addButton);
+        addRemovePanel.add(removeButton);
 
 
         add(addRemovePanel);
